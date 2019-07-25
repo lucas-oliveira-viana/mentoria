5392acacf67a57ae9584c2988b1ddb4221ba8b77
Lucas Viana <lucas.viana@db1.com.br>
2019-07-25 15:31:46 -0300

    contact pronto

--- js/slide.js
+++ js/slide.js
@@ -1,13 +1,13 @@
 $(document).ready(() => {
 
     const tatsumaki = {
-        heroName: "Classe S #2 - Senritsu no Tatsumaki (Tornado do Terror)",
+        heroName: "Classe #2 - Senritsu no Tatsumaki (Tornado do Terror)",
         rgb: `rgb(17, 172, 69`,
         opacity: `1`,
         descriptionContent: `É a mulher mais poderosa da Associação de Heróis. Dona de uma personalidade bastante forte, Tatsumaki está quase sempre de mau humor.
         A Tornado tem poderes paranormais telecinéticos e consegue executar ataques psíquicos incríveis! 
         Ela ainda consegue criar barreiras de energia e controlar o chi (a "força da vida").`,
-        heroBackground: `linear-gradient(to right, rgba(0, 0, 0, 0.7) 30%, rgba(0, 0, 0, 0.7) 100%), url(./img/tatsumaki.jpg)`
+        heroBackground: `linear-gradient(to right, rgba(0, 0, 0, 0.6) 30%, rgba(0, 0, 0, 0.6) 100%), url(./img/tatsumaki.jpg)`
     }
 
     const bang = {
@@ -16,7 +16,7 @@ $(document).ready(() => {
         opacity: `1`,
         descriptionContent: `Também conhecido por Bang, este herói tem uma força física incrível, apesar da sua idade avançada em comparação aos outros (tem mais de 80 anos).
         Além disso, Fang desenvolveu o seu sexto sentido, sendo capaz de atacar ou se defender mesmo sem o uso da visão, audição e olfato, por exemplo.`,
-        heroBackground: `linear-gradient(to right, rgba(0, 0, 0, 0.7) 30%, rgba(0, 0, 0, 0.7) 100%), url(./img/bang.png)`
+        heroBackground: `linear-gradient(to right, rgba(0, 0, 0, 0.6) 30%, rgba(0, 0, 0, 0.6) 100%), url(./img/bang.png)`
     }
 
     const atomicSamurai = {
@@ -25,7 +25,7 @@ $(document).ready(() => {
         opacity: `1`,
         descriptionContent: `Também chamado de Kamikaze, este formidável espadachim é um dos heróis mais orgulhosos da Associação. 
         Este super-herói é um mestre com a espada, sendo que o seu estilo de luta se baseia no uso desta arma.`,
-        heroBackground: `linear-gradient(to right, rgba(0, 0, 0, 0.7) 30%, rgba(0, 0, 0, 0.7) 100%), url(./img/atomic-samurai.jpg)`
+        heroBackground: `linear-gradient(to right, rgba(0, 0, 0, 0.6) 30%, rgba(0, 0, 0, 0.6) 100%), url(./img/atomic-samurai.jpg)`
     }
 
     const heroList = [tatsumaki, bang, atomicSamurai];
@@ -36,7 +36,9 @@ $(document).ready(() => {
 
         $("#ranking").css("background", heroInformations.heroBackground);
         $("#ranking").css("background-size", "cover");
-        $("#ranking").css("border-top", `15px solid ${heroInformations.rgb})`)
+        $("#ranking").css("border-top", `5px solid ${heroInformations.rgb})`)
+        $("#ranking").css("border-bottom", `5px solid ${heroInformations.rgb})`)
+        $("#ranking").css("background-position-x", `center`)
 
         $(".ranking__position").text(heroInformations.heroName);
         $(".ranking__description").append(descriptionHtml);
